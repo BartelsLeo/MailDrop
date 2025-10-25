978339eec84e7da54d565067dfc3a2a6b79fc92c
Leonard Bartels <leonard.bartels@posteo.de>
2025-10-25 21:41:50 +0200

    Group "MailDrop" mit Button "Mail ablegen" in "Start" Tab erfolgreich erzeugt

---
+++ MailDropRibbon.vb
@@ -0,0 +1,27 @@
+Imports System.Runtime.InteropServices
+Imports Microsoft.Office.Core
+
+<ComVisible(True)>
+Public Class MailDropRibbon
+    Implements IRibbonExtensibility
+
+    Private ribbon As IRibbonUI
+
+    Public Function GetCustomUI(ribbonID As String) As String Implements IRibbonExtensibility.GetCustomUI
+        Dim asm = System.Reflection.Assembly.GetExecutingAssembly()
+        Using stream = asm.GetManifestResourceStream("MailDrop.MailDropRibbon.xml")
+            Using reader As New System.IO.StreamReader(stream)
+                Return reader.ReadToEnd()
+            End Using
+        End Using
+    End Function
+
+    Public Sub MailAblegen_Click(control As IRibbonControl)
+        ' Delegiert an ThisAddIn
+        Globals.ThisAddIn.MailAblegen_Click(control)
+    End Sub
+
+    Public Sub OnLoad(ribbonUI As IRibbonUI)
+        Me.ribbon = ribbonUI
+    End Sub
+End Class
---
+++ ThisAddIn.vb
@@ -0,0 +1,27 @@
+﻿Imports Microsoft.Office.Tools.Ribbon
+Imports System.Runtime.InteropServices
+Imports Microsoft.Office.Core
+
+Public Class ThisAddIn
+    Private ribbonObj As MailDropRibbon
+
+    Private Sub ThisAddIn_Startup() Handles Me.Startup
+
+    End Sub
+
+    Private Sub ThisAddIn_Shutdown() Handles Me.Shutdown
+
+    End Sub
+
+    Protected Overrides Function CreateRibbonExtensibilityObject() As IRibbonExtensibility
+        ribbonObj = New MailDropRibbon()
+        Return ribbonObj
+    End Function
+
+    ' Callback for Ribbon button
+    Public Sub MailAblegen_Click(control As Object)
+        ' Hier die gewünschte Logik einfügen
+        System.Windows.Forms.MessageBox.Show("Mail ablegen geklickt!")
+    End Sub
+
+End Class
